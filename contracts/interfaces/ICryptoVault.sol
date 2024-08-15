eb0a79ff5fa5230e08a43eb6cc73a8aa8947f233
Ivan Ferrera <ivanferrera@gmail.com>
2024-08-15 11:44:05 -0400

    updated solidity version

--- contracts/interfaces/ICryptoVault.sol
+++ contracts/interfaces/ICryptoVault.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.24;
 
 interface CryptoVaultInterface {
-  function deposit(address,uint256) external;
-  function withdraw(address,uint256,address) external;
-     function isAssetStored(
-        address ,
-        uint256
-    ) external view returns (bool) ;
+    function deposit(address, uint256) external;
 
-    function attachReceiptToNFT(address,uint256,uint256) external;
+    function withdraw(address, uint256, address) external;
+
+    function isAssetStored(address, uint256) external view returns (bool);
+
+    function attachReceiptToNFT(address, uint256, uint256) external;
 }
