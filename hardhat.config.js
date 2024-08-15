2cc44c9302167592076b3cb05e2f29a09cbf7c88
Ivan Ferrera <ivanferrera@gmail.com>
2024-08-15 11:48:02 -0400

    updated to work with QuickNode

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,29 +1,30 @@
-require("dotenv").config();
-require("@nomiclabs/hardhat-waffle");
+// hardhat.config.js
 
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	networks: {
-		goerli: {
-			url: `https://goerli.infura.io/v3/5d5d153cab5c46f193cbb81bbddb5aa5`,
-			accounts: [
-				"e753ec0394dcaf65f0b2d65b57e3e6d77b4919e0557f89e2b831b182d08dfba1",
-			],
-		},
-		local: {
-			chainId: 1337,
-			url: "http://127.0.0.1:7545",
-			accounts: [
-				"e753ec0394dcaf65f0b2d65b57e3e6d77b4919e0557f89e2b831b182d08dfba1",
-			],
-		},
-	},
-	solidity: {
-		version: "0.8.19", // Your preferred Solidity compiler version
-		settings: {
-			optimizer: {
-				enabled: true,
-				runs: 200,
-			},
-		},
-	},
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: process.env.QUICKNODE_SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    holesky: {
+      url: process.env.QUICKNODE_HOLESKY_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    mainnet: {
+      url: process.env.QUICKNODE_MAINNET_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
