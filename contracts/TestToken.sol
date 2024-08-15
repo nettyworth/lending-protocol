558d87579b4991ae77e8a6e2ca92204c4077acc2
Ivan Ferrera <ivanferrera@gmail.com>
2024-08-15 11:43:45 -0400

    updated ReentrancyGuard and solidity version

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 // solhint-disable-next-line
-pragma solidity 0.8.19;
+pragma solidity ^0.8.24;
 import "./ERC721A.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract TestToken is ERC721A, ReentrancyGuard,Ownable {
+contract TestToken is ERC721A, ReentrancyGuard, Ownable {
     using Strings for uint256;
     // Public attributes for Manageable interface
     string public project;
@@ -20,7 +20,7 @@ contract TestToken is ERC721A, ReentrancyGuard,Ownable {
         string memory _name,
         string memory _symbol,
         uint256 _maxSupply
-    ) ERC721A(_name, _symbol) {
+    ) ERC721A(_name, _symbol) Ownable(msg.sender) {
         project = _project;
         maxSupply = _maxSupply;
     }
@@ -34,13 +34,9 @@ contract TestToken is ERC721A, ReentrancyGuard,Ownable {
         baseURI = newBaseURI;
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
         require(
             _exists(tokenId),
             "ERC721Metadata: URI query for nonexistent token"
@@ -75,6 +71,4 @@ contract TestToken is ERC721A, ReentrancyGuard,Ownable {
     function _startTokenId() internal view virtual override returns (uint256) {
         return 1;
     }
-
-
 }
