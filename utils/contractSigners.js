4360d1b1d618f45504cb6d880706151c26c8cb7d
hadid12 <hadidtaseer@gmail.com>
2024-09-10 19:54:37 +0500

    update admin address in intializer

--- utils/contractSigners.js
+++ utils/contractSigners.js
@@ -1,8 +1,10 @@
 const { ethers } = require("ethers"); // Import the ethers library
 
-const SECRET_SIGNER_PRIVATE_KEY =
-  "45368821537686a5a8afeb4ee89b127af53cee27ce2850ca272764181f5b6b87"; // Ensure the private key is prefixed with '0x'
+// const SECRET_SIGNER_PRIVATE_KEY =
+//   "45368821537686a5a8afeb4ee89b127af53cee27ce2850ca272764181f5b6b87"; // Ensure the private key is prefixed with '0x'
 
+const SECRET_SIGNER_PRIVATE_KEY =
+  "0xd5195ab71a72664b32af2ae55c1163802bcf1f0e3d1b76d19ef50d55bcb69cf7"; // Ensure the private key is prefixed with '0x';
 // const SECRET_SIGNER_PRIVATE_KEY =
 //   "cad64b32ccae3f80a422387aa8e709c2fb321aa77738b9596f938ef9fac2a764";
 
@@ -175,6 +177,9 @@ function sign(encoded) {
   return signature;
 }
 
+// 0x2c92485bc27c171f681de34855f7079653ce4782e4097925a17c728092742086615ec035b53c626d5245ee2ebca8e65992ae44a30961bd9e706ab6da041d25501b;
+// 0x943364c62bdb9598c586141f79afabde533444d3b669609aed2b53de3bc97fae11ba2c8e6eb17ec9968a4932b3c4a48415bf6d025810223e3b6625b62a4d47081b;
+
 // nftContract: 0x358aa13c52544eccef6b0add0f801012adad5ee3;
 // tokenId = 1;
 // borrower = 0xab8483f64d9c6d1ecf9b849ae677dd3315835cb2;
@@ -192,9 +197,7 @@ const main = async () => {
   //     223898,
   //     "0xa611531661B5649688605a16ca7a245980F69A99"
   //   );
-
   //   console.log("Sign Offer signature", getsign);
-
   //   const getacceptLoanRequest = await acceptLoanRequest(
   //     2, // TokenId
   //     "0xe9318493c0fd30140afa8ecc47467b36da23855e", //nftContractAddress,
@@ -205,29 +208,23 @@ const main = async () => {
   //     1728482023, //loanDuration,
   //     54321 //nonce
   //   );
-
   //   console.log("getacceptLoanRequest", getacceptLoanRequest);
-
-  const getacceptLoanCollectionOffer = await acceptLoanCollectionOffer(
-    "0xe9318493c0fd30140afa8ecc47467b36da23855e", //     collectionAddress,
-    "0x4E2b47AdCFcEB40c0bb1Dd283a7E539B26CFF8c4", //     erc20TokenAddress,
-    "0x2DC67345a60b5f2BA1d4f4bB661F6Ec31AF6B061", //     lender,
-    10000000000000000000n, //     loanAmount,
-    2000, //     interestRate,
-    1728482023, //     loanDuration,
-    56789 //     nonce
-  );
-
-  console.log("getacceptLoanCollectionOffer", getacceptLoanCollectionOffer);
-
+  // const getacceptLoanCollectionOffer = await acceptLoanCollectionOffer(
+  //   "0xe9318493c0fd30140afa8ecc47467b36da23855e", //     collectionAddress,
+  //   "0x4E2b47AdCFcEB40c0bb1Dd283a7E539B26CFF8c4", //     erc20TokenAddress,
+  //   "0x2DC67345a60b5f2BA1d4f4bB661F6Ec31AF6B061", //     lender,
+  //   10000000000000000000n, //     loanAmount,
+  //   2000, //     interestRate,
+  //   1728482023, //     loanDuration,
+  //   56789 //     nonce
+  // );
+  // console.log("getacceptLoanCollectionOffer", getacceptLoanCollectionOffer);
   //   const deposit = await signDeposit(
   //     "0x482CDDDff6adb44C0da472cfAA939fF615663025",
   //     1,
   //     "0x529Ece8c1995be3D4839912551565F1531037737"
   //   );
-
   //   console.log("Deposit Signature", deposit);
-
   //   const createloan = await signCreateLoan(
   //     1,
   //     "0x482CDDDff6adb44C0da472cfAA939fF615663025",
@@ -239,7 +236,6 @@ const main = async () => {
   //     200202,
   //     "0x529ece8c1995be3d4839912551565f1531037737"
   //   );
-
   //   console.log("Create Loan Signature", createloan);
 };
 
